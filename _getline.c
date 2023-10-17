1cee1252c4c597a4d8befcbf0fd74b92491ca36f
MohamedAbd-elrady <mmohammedabdelrady@gmail.com>
2023-10-18 01:50:19 +0300

    _getline

---
+++ _getline.c
@@ -0,0 +1,142 @@
+#include "shell.h"
+
+/**
+ * _strcpy - copies the string pointed to by src,
+ * including the terminating null byte (\0),
+ * to the buffer pointed to by dest
+ * @dest: copy source to this buffer
+ * @src: this is the source to copy
+ * Return: copy of original source
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i, len;
+
+	for (len = 0; src[len] != '\0'; len++)
+		;
+
+	for (i = 0; i <= len; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
+
+/**
+ * _strcat - concatenate two strings
+ * @dest: string to be appended to
+ * @src: string to append
+ * Return: concatenated string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int len = 0;
+	int len2 = 0;
+	int total_len = 0;
+	int j = 0;
+
+	while (dest[len] != '\0')
+	{
+		len++;
+		total_len++;
+	}
+	while (src[len2] != '\0')
+	{
+		len2++;
+		total_len++;
+	}
+
+	dest = _realloc(dest, len, sizeof(char) * total_len + 1);
+
+	while (src[j] != '\0')
+	{
+		dest[len] = src[j];
+		len++;
+		j++;
+	}
+	dest[len] = '\0';
+
+	return (dest);
+}
+
+/**
+ * _realloc - allocate memory and set all values to 0
+ * @ptr: pointer to the memory previously allocated (malloc(old_size))
+ * @old_size: size previously allocated
+ * @new_size: new size to reallocate
+ * Return: pointer to reallocated memory
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *p;
+	unsigned int i;
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (ptr == NULL)
+	{
+		p = malloc(new_size);
+		if (p == NULL)
+			return (NULL);
+		else
+			return (p);
+	}
+
+	p = malloc(new_size);
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < old_size && i < new_size; i++)
+		*((char *)p + i) = *((char *)ptr + i);
+	free(ptr);
+
+	return (p);
+}
+
+/**
+ * get_line - stores into malloced buffer the user's command into shell
+ * @str: buffer
+ * Return: number of characters read
+ */
+size_t _getline(char **str)
+{
+	ssize_t i = 0, size = 0, t = 0, t2 = 0, n = 0;
+	char buff[1024];
+
+	while (t2 == 0 && (i = read(STDIN_FILENO, buff, 1024 - 1)))
+	{
+		if (i == -1)
+			return (-1);
+
+		buff[i] = '\0';
+
+		n = 0;
+		while (buff[n] != '\0')
+		{
+			if (buff[n] == '\n')
+				t2 = 1;
+			n++;
+		}
+
+		if (t == 0)
+		{
+			i++;
+			*str = malloc(sizeof(char) * i);
+			*str = _strcpy(*str, buff);
+			size = i;
+			t = 1;
+		}
+		else
+		{
+			size += i;
+			*str = _strcat(*str, buff);
+		}
+	}
+	return (size);
+}
