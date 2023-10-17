9606eb1d2c78af6eba146dc25f0971c50894d2e7
mohamed_kamal <kmido9729@gmail.com>
2023-10-18 00:42:10 +0200

    kamal---string.c file

---
+++ string.c
@@ -0,0 +1,52 @@
+#include "shell.h"
+
+/**
+ * _strcmp - compare two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: int that tells num spaces in between, 0 if exactly the same string
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (*(s1 + i) == *(s2 + i) && *(s1 + i))
+		i++;
+
+	if (*(s2 + i))
+		return (*(s1 + i) - *(s2 + i));
+	else
+		return (0);
+}
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter
+ * @str: string to duplicate
+ * Return: pointer to duplicated string in allocated memory
+ */
+char *_strdup(char *str)
+{
+	char *duplicate_str;
+	int i, len = 0;
+
+	if (str == NULL) /* validate str input */
+		return (NULL);
+
+	while (*(str + len))
+		len++;
+	len++; /* add null terminator to length */
+
+	duplicate_str = malloc(sizeof(char) * len); /* allocate memory */
+	if (duplicate_str == NULL)
+		return (NULL);
+
+	i = 0;
+	while (i < len)
+	{
+		*(duplicate_str + i) = *(str + i);
+		i++;
+	}
+
+	return (duplicate_str);
+}
